5fc233134a9d59c36edcb15cc9a4674b8317eb9f
honpery <honpery@gmail.com>
2016-09-07 01:29:11 +0800

    完成上传excel接口

--- api/resource/index.js
+++ api/resource/index.js
@@ -7,14 +7,18 @@ const _router = express.Router();
 const resourceActions = require('./actions');
 const xlsx = require('xlsx');
 const multer = require('multer');
-const upload = multer({dest: './uploads/'});
+const upload = multer({ dest: './uploads/' });
+const fs = require('fs');
+const path = require('path');
+const xres = require('../common/xres');
+
 
 // 获取资源
-_router.get('/:resource_id',(req, res) => {
+_router.get('/:resource_id', (req, res) => {
     let {resource_id} = req.params;
     resourceActions.detail(resource_id, {}, (result) => {
         if (result.type === 'json') {
-            res.json(xres({CODE: 0}, result));
+            res.json(xres({ CODE: 0 }, result));
         }
         // if (result.type === '') {
 
@@ -23,8 +27,42 @@ _router.get('/:resource_id',(req, res) => {
 });
 
 // 上传资源
-_router.post('/', upload.array(), (req, res) => {
-    console.log(req.files);
+_router.post('/', upload.single('file'), (req, res) => {
+    let {type} = req.body;
+    if (type === 'xlsx') {
+        let workbook = xlsx.readFile(req.file.path);
+    
+        let resData = {
+            filename: req.file.filename,
+            data: []
+        };
+
+        workbook.SheetNames.map((sheet, index, arr) => {
+            let sheetData = workbook.Sheets[sheet];
+
+            let currentSheetData = {
+                product_type: sheet,
+                fields: [],
+                names: [],
+                values: []
+            };
+
+            for (let cell in sheetData) {
+                if (cell.charAt(1) === '1') {
+                    currentSheetData.fields.push(sheetData[cell].v);
+                } else if (cell.charAt(1) === '2') {
+                    currentSheetData.names.push(sheetData[cell].v);
+                } else {
+                    let current = currentSheetData.values[Number(cell.substr(1)) - 3] = [];
+                    current.push(sheetData[cell].v);
+                }
+                
+            }
+            resData.data.push(currentSheetData);
+        });
+        
+        res.json(xres({CODE: 0}, resData));
+    }
 });
 
 module.exports = _router;
