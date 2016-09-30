d84ab9f348de6367ba153f90fd79abcb673a207e
honpery <honpery@gmail.com>
2016-10-01 00:12:35 +0800

    完成模板接口

--- api/template/index.js
+++ api/template/index.js
@@ -4,28 +4,55 @@
 const _router = require('express').Router();
 const templateModel = require('../common/xmodel')('template');
 const xres = require('../common/xres');
+const xfilter = require('../common/xfilter');
 
 module.exports = _router
 
-    // 列表 / 详情
-    .get('/:template_id?', (req, res) => {
-        
+    // 列表
+    .get('/', (req, res) => {
+        let condition = {};
+        templateModel.list(condition, (result) => {
+            res.json(xres({ code: 0 }, xfilter(result, '_id', 'name', 'create_time', 'update_time')));
+        });
+    })
+
+
+    // 详情
+    .get('/:template_id', (req, res) => {
+        let {template_id} = req.params;
+        templateModel.detail(template_id, {}, (result) => {
+            res.json(xres({ code: 0 }, xfilter(result, '_id', 'name', 'template', 'create_time', 'update_time')));
+        });
     })
 
 
     // 添加
     .post('/', (req, res) => {
-
+        let newData = xfilter(req.body, 'name', 'template');
+        templateModel.create(newData, (result) => {
+            res.json(xres({ code: 0 }, xfilter(result, '_id', 'create_time')));
+        });
     })
 
 
     // 修改
     .patch('/:template_id', (req, res) => {
+        let {template_id} = req.params;
 
+        let newData = xfilter(req.body, 'name', 'template');
+        newData.update_time = Date.now();
+
+        templateModel.update(template_id, newData, (result) => {
+            res.json(xres({ code: 0 }, { _id, update_time: newData.update_time }));
+        })
     })
 
 
     // 删除 
     .delete('/:template_id', (req, res) => {
+        let {template_id} = req.params;
 
+        templateModel.delete(template_id, (result) => {
+            res.json(xres({ code: 0 }));
+        });
     });
