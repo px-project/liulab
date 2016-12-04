4236f5d31d0a9d66ed5dc122139f83784da8af0d
honpery <honpery@gmail.com>
2016-12-04 23:27:38 +0800

    前端container合并。

--- admin/src/containers/app/index.jsx
+++ admin/src/containers/app/index.jsx
@@ -2,12 +2,13 @@
  * 顶级容器
  */
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from '../../actions/';
 import {Header, Sidebar} from '../../components/app/';
 import './style.scss';
 
 export class AppContainer extends Component {
-	componentWillMount () {
-	}
 
 	render() {
 		return (
@@ -16,10 +17,21 @@ export class AppContainer extends Component {
 				<div id="main">
 					<Header {...this.props}></Header>
 					<div className="page">
-						{this.props.children}
+						{React.cloneElement(this.props.children, this.props)}
 					</div>
 				</div>
 			</div>
 		);
 	}
+
 }
+
+function mapStateToProps (state) {
+	return state;
+}
+
+function mapDispatchToProps (dispatch) {
+	return bindActionCreators(Object.assign({}, ...Object.keys(actions).map(key => ({[key]: actions[key]}))), dispatch);
+}
+
+export const App = connect(mapStateToProps, mapDispatchToProps)(AppContainer);
--- admin/src/routes/index.js
+++ admin/src/routes/index.js
@@ -8,22 +8,20 @@ import * as components from '../components/';
 import routes from '../config/routes.json';
 import { createHistory } from 'history';
 import { toCamcel } from '../utils/';
+
 const history = useRouterHistory(createHistory)({ basename: window.location.origin + '/' });
 
 export default class Routes extends Component {
     render() {
         return (
             <Router history={history}>
-                <Route path="/" component={containers.AppContainer}>
-                    <IndexRedirect to="/index" />
+                <Route path="/" component={containers.App}>
+                    <IndexRedirect to="/index"/>
 
                     {routes
-                        .filter((topLevel) => window.permission.filter(item => item.module === topLevel.path)[0].allow)
+                        .filter(topLevel => window.permission.filter(item => item.module === topLevel.path)[0].allow)
                         .map((topLevel, index) => (
-                            <Route path={topLevel.path} key={index} component={containers[toCamcel(true, topLevel.path, 'app')]} {...this.props}>
-
-                                <IndexRoute component={components[toCamcel(true, topLevel.path, 'component')]} {...this.props} />
-
+                            <Route path={topLevel.path} key={index} component={components[toCamcel(true, topLevel.path, 'component')]}>
                                 {topLevel.children && topLevel.children.map((child, index) => (
                                     <Route path={child.path} key={index} component={components[child.componentName]} {...this.props}></Route>
                                 ))}
