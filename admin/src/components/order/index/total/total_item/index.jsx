1598accf475096abc97568848b2ee55f46a2a597
honpery <honpery@gmail.com>
2016-12-19 01:02:27 +0800

    complate order total page.

--- admin/src/components/order/index/total/total_item/index.jsx
+++ admin/src/components/order/index/total/total_item/index.jsx
@@ -12,45 +12,25 @@ import './style.scss';
 export class OrderTotalItemComponent extends Component {
 
     componentDidMount() {
-        console.log(this.refs.chart)
-        // new Chart(this.refs.chart, {
-        //     type: 'doughnut',
-        //     data: [1, 2, 3]
-        // })
-        var myChart = new Chart(this.refs.chart, {
-            type: 'bar',
+
+        let {total} = this.props.order;
+
+        Chart.defaults.global.legend.display = false;
+        new Chart(this.refs.chart, {
+            type: 'doughnut',
             data: {
-                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+                labels: Object.keys(ORDER_STATUS).map(status => ORDER_STATUS[status].name),
+                datasets: [
+                    {
+                        data: Object.keys(ORDER_STATUS).map(status => total[status]),
+                        backgroundColor: Object.keys(ORDER_STATUS).map(status => ORDER_STATUS[status].color),
+                        fontSize: '20px'
+
+                    }]
             },
             options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
+                responsive: true,
+                maintainAspectRatio: true
             }
         });
     }
@@ -61,7 +41,9 @@ export class OrderTotalItemComponent extends Component {
             <li className="order-total-item">
                 <div className="info">
                     <div className="order-id">{order.order_id}</div>
-                    <div className="chart" ref="chart"></div>
+                    <div className="chart">
+                        <canvas ref="chart"></canvas>
+                    </div>
                     <div className="user-time">
                         <div className="user">{order.create_user}</div>
                         <div className="create-time">{moment(order.create_time).format('YYYY/MM/DD')}</div>
@@ -72,44 +54,5 @@ export class OrderTotalItemComponent extends Component {
                 </div>
             </li>
         );
-
-
-        // return (
-        //     <div className="order-total">
-        //         {!order.fetching ? (
-        //             order.items.length ? (
-        //                 <table className="ui table">
-        //                     <thead>
-        //                         <tr>
-        //                             <th>序号</th>
-        //                             <th>订单号</th>
-        //                             <th>概述</th>
-        //                             <th>订货人</th>
-        //                             <th>创建时间</th>
-        //                             <th>操作</th>
-        //                         </tr>
-        //                     </thead>
-
-        //                     <tbody>
-        //                         {order.items.map((order_id, order_index) => (
-        //                             <tr key={order_index}>
-        //                                 <td>{order_index + 1}</td>
-        //                                 <td>{entities[order_id].order_id}</td>
-        //                                 <td className="total">{Object.keys(ORDER_STATUS).map((status, status_index) => (
-        //                                     <span className="total-sec" key={status_index}><span className="name">{ORDER_STATUS[status]}</span><span>({entities[order_id].total[status]})</span></span>
-        //                                 ))}</td>
-        //                                 <td>{entities[order_id].create_user}</td>
-        //                                 <td>{moment(entities[order_id].create_time).format('YYYY-MM-DD HH:mm:ss')}</td>
-        //                                 <td>
-        //                                     <Link to={`/order/${entities[order_id].order_id}`}>详情</Link>
-        //                                 </td>
-        //                             </tr>
-        //                         ))}
-        //                     </tbody>
-        //                 </table>
-        //             ) : (<div className="nodata">暂无数据</div>)
-        //         ) : (<div className="loading">加载中</div>)}
-        //     </div>
-        // );
     }
 }
--- admin/src/constants/order.js
+++ admin/src/constants/order.js
@@ -3,10 +3,28 @@
  */
 
 export const ORDER_STATUS = {
-	pending: '待审核',
-	pended: '已审核',
-	failed: '未通过',
-	processing: '订货中',
-	success: '已到货',
-	cancel: '已取消'
+	pending: {
+		name: '待审核',
+		color: 'rgb(255, 99, 132)'
+	},
+	pended: {
+		name: '已审核',
+		color: 'rgb(54, 162, 235)'
+	},
+	failed: {
+		name: '未通过',
+		color: 'rgb(255, 206, 86)'
+	},
+	processing: {
+		name: '订货中',
+		color: 'rgb(75, 192, 192)'
+	},
+	success: {
+		name: '已到货',
+		color: 'rgb(153, 102, 255)'
+	},
+	cancel: {
+		name: '已取消',
+		color: 'rgb(100, 100, 100)'
+	}
 };
