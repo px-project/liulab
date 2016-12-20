f0d49a61de549a4accddaf659adcba643039a0b7
honpery <honpery@gmail.com>
2016-12-20 22:03:49 +0800

    edit page.

--- admin/src/components/order/detail/total/index.jsx
+++ admin/src/components/order/detail/total/index.jsx
@@ -4,12 +4,21 @@
 import React, { Component } from 'react';
 import './style.scss';
 import moment from 'moment';
-import * as consts from '../../../constants/';
+import * as consts from '../../../../constants/';
 import classname from 'classname';
 
 
 export class OrderDetailComponent extends Component {
-	render () {
+	componentWillMount() {
+		let {xhttp, params} = this.props;
+		let {order_id} = params;
+		
+		xhttp({action: 'detail', api: 'order', params: [order_id]});
+	}
+
+
+
+	render() {
 		return (<div>order detail</div>)
 	}
 	// componentWillMount() {
